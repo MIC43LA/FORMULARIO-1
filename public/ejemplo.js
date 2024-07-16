e4326d0889758a98473b0c3ea688a0fae99b3d6e
Micaela <micaflaherty18@gmail.com>
2024-07-16 02:15:06 -0300

    ok

--- public/ejemplo.js
+++ public/ejemplo.js
@@ -29,47 +29,47 @@ function enviar() {
     let form= document.getElementById("form").reset()
     }
 
-function oscuro() {
-    let input = document.getElementsByTagName("input")
-    let pais = document.getElementsByTagName("select").item(0)
-    let fondo = document.getElementsByTagName("body").item(0)
-    let boton1 = document.getElementById("boton-1")
-    let boton2 = document.getElementById("boton-2")
-    let title = document.getElementsByClassName("titulo").item(0)
-    let formulario = document.getElementsByClassName("formulario").item(0)
-    if (HC == 0) {
-        Array.from(input).forEach(c => {
-            c.classList.add("inputHC")
-        })
-        pais.classList.add("inputHC")
-        fondo.classList.add("bodyHC")
-        boton1.classList.add("botonHC")
-        boton2.classList.add("botonHC")
-        title.classList.add("tituloHC")
-        formulario.classList.add("formHC")
+// function oscuro() {
+//     let input = document.getElementsByTagName("input")
+//     let pais = document.getElementsByTagName("select").item(0)
+//     let fondo = document.getElementsByTagName("body").item(0)
+//     let boton1 = document.getElementById("boton-1")
+//     let boton2 = document.getElementById("boton-2")
+//     let title = document.getElementsByClassName("titulo").item(0)
+//     let formulario = document.getElementsByClassName("formulario").item(0)
+//     if (HC == 0) {
+//         Array.from(input).forEach(c => {
+//             c.classList.add("inputHC")
+//         })
+//         pais.classList.add("inputHC")
+//         fondo.classList.add("bodyHC")
+//         boton1.classList.add("botonHC")
+//         boton2.classList.add("botonHC")
+//         title.classList.add("tituloHC")
+//         formulario.classList.add("formHC")
 
-        HC = 1
-    }
-    else {
-        console.log(input)
-        Array.from(input).forEach(c => {
-            c.classList.remove("inputHC")
+//         HC = 1
+//     }
+//     else {
+//         console.log(input)
+//         Array.from(input).forEach(c => {
+//             c.classList.remove("inputHC")
 
-        })
-        pais.classList.remove("inputHC")
-        fondo.classList.remove("bodyHC")
-        boton1.classList.remove("botonHC")
-        boton2.classList.remove("botonHC")
-        title.classList.remove("tituloHC")
-        formulario.classList.remove("formHC")
+//         })
+//         pais.classList.remove("inputHC")
+//         fondo.classList.remove("bodyHC")
+//         boton1.classList.remove("botonHC")
+//         boton2.classList.remove("botonHC")
+//         title.classList.remove("tituloHC")
+//         formulario.classList.remove("formHC")
 
 
-        HC = 0
-    }
-}
+//         HC = 0
+//     }
+// }
 
 let elem = document.getElementById("boton-1")
 elem.addEventListener("click", enviar);
 
-let elemt = document.getElementById("boton-2")
-elemt.addEventListener("click", oscuro);
+//let elemt = document.getElementById("boton-2")
+//elemt.addEventListener("click", oscuro);
